05c3d9569ff51ad1cbec5671e1e0e7b0de2216b3
samuel <smfikiru@gmail.com>
2026-08-23 19:33:17 +0300

    fixes

--- api/geoData.js
+++ api/geoData.js
@@ -2,7 +2,7 @@ export default{
     async fetch(request){
         const url = new URL(request.url);
 
-        const placeName = url.searchParams.get('q');
+        const locationinput = url.searchParams.get('q');
 
         const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${locationinput}&limit=5&appid=${process.env.OPENWEATHER_API_KEY}`);
         const data = await response.json();
